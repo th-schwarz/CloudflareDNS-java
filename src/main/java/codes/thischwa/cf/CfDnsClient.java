0e4d68d719657bb226e06c508782408aa5de3554
Thilo Schwarz <osp@thischwa.codes>
2025-06-09 14:12:35 +0200

    Update example IP address in Javadoc and adjust GitLab CI rules

--- src/main/java/codes/thischwa/cf/CfDnsClient.java
+++ src/main/java/codes/thischwa/cf/CfDnsClient.java
@@ -33,7 +33,7 @@ import lombok.extern.slf4j.Slf4j;
  *     System.out.println("Record Type: " + record.getType() + ", Value: " + record.getContent())
  * );
  * // Create a record for the subdomain "api"
- * RecordEntity created = client.recordCreateSld(zone, "api", 60, RecordType.A, "192.168.2);
+ * RecordEntity created = client.recordCreateSld(zone, "api", 60, RecordType.A, "192.168.3);
  * System.out.println("Created Record ID: " + created.getId());
  * </code></pre>
  */
