25228af9f052bda2113919e38fd82f63621f9d52
Thilo Schwarz <osp@thischwa.codes>
2026-06-28 18:20:39 +0200

    Generate Javadocs for `BatchResponse` and `RecordType` classes in the `codes.thischwa.cf.model` package.

---
+++ docs/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"codes.thischwa.cf"},{"l":"codes.thischwa.cf.fluent"},{"l":"codes.thischwa.cf.model"}];updateSearchResults();
